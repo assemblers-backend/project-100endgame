a188bac79267594f99df9362f773c55a8b09ee8d
unknown <jshyun912@naver.com>
2026-05-28 18:09:43 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/io/assemblers/project100endgame/Project100endgameApplication.java
@@ -0,0 +1,12 @@
+package io.assemblers.project100endgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project100endgameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project100endgameApplication.class, args);
+	}
+}
---
+++ src/test/java/io/assemblers/project100endgame/Project100endgameApplicationTests.java
@@ -0,0 +1,13 @@
+package io.assemblers.project100endgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project100endgameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
