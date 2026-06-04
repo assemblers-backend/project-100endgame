f4457156ee459006d283a0bba7ac858a235c75ae
한승우 <tmddn_00@naver.com>
2026-06-04 16:15:04 +0900

    feature: 공통 응답 구조 구현

--- src/main/java/io/assemblers/project100endgame/item/controller/ItemController.java
+++ src/main/java/io/assemblers/project100endgame/item/controller/ItemController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.assemblers.project100endgame.common.response.ApiResponse;
 import io.assemblers.project100endgame.item.dto.ItemResponse;
 import io.assemblers.project100endgame.item.service.ItemService;
 import lombok.RequiredArgsConstructor;
@@ -19,12 +20,12 @@ public class ItemController {
 	private final ItemService itemService;
 
 	@GetMapping
-	public List<ItemResponse> getItems() {
-		return itemService.getItems();
+	public ApiResponse<List<ItemResponse>> getItems() {
+		return ApiResponse.success(itemService.getItems());
 	}
 
 	@GetMapping("/{id}")
-	public ItemResponse getItem(@PathVariable Long id) {
-		return itemService.getItem(id);
+	public ApiResponse<ItemResponse> getItem(@PathVariable Long id) {
+		return ApiResponse.success(itemService.getItem(id));
 	}
 }
