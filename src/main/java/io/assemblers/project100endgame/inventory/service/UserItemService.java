884878c6d599d3deae1d7e0157c479aefd76cf16
한승우 <tmddn_00@naver.com>
2026-06-06 22:25:02 +0900

    feat: UserItemService 구현 - getInventory(현재 인벤토리 정보 및 아이템 정보들)

---
+++ src/main/java/io/assemblers/project100endgame/inventory/service/UserItemService.java
@@ -0,0 +1,25 @@
+package io.assemblers.project100endgame.inventory.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import io.assemblers.project100endgame.inventory.dto.UserItemResponse;
+import io.assemblers.project100endgame.inventory.repository.UserItemRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class UserItemService {
+
+	private final UserItemRepository userItemRepository;
+
+	public List<UserItemResponse> getInventory(Long userId) {
+		return userItemRepository.findAllByUserIdWithItem(userId)
+			.stream()
+			.map(UserItemResponse::from)
+			.toList();
+	}
+}
