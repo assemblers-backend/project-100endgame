2951d293cab48f78fc06ebd0494eafc61532b048
한승우 <tmddn_00@naver.com>
2026-06-07 16:50:52 +0900

    test: UserItem Post 테스트 성공

--- src/main/java/io/assemblers/project100endgame/auth/config/SecurityConfig.java
+++ src/main/java/io/assemblers/project100endgame/auth/config/SecurityConfig.java
@@ -70,6 +70,7 @@ public class SecurityConfig {
 				.authenticated()
 
 				// 인벤토리
+				// TODO: 인벤토리 권한 authenticated으로 수정
 				.requestMatchers(HttpMethod.GET, "/api/v1/users/me/inventory")
 				.permitAll()
 				.requestMatchers(HttpMethod.POST, "/api/v1/users/me/inventory/pickup")
