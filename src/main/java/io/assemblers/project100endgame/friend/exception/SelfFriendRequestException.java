833ea48cd3cba152cdadc543d54bb413a99fb8a0
한승우 <tmddn_00@naver.com>
2026-06-08 10:10:47 +0900

    feat: 친구 요청 보내기 예외

---
+++ src/main/java/io/assemblers/project100endgame/friend/exception/SelfFriendRequestException.java
@@ -0,0 +1,7 @@
+package io.assemblers.project100endgame.friend.exception;
+
+public class SelfFriendRequestException extends RuntimeException {
+	public SelfFriendRequestException() {
+		super("자기 자신에게 친구 요청을 보낼 수 없습니다.");
+	}
+}
